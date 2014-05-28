179fe373b8cd3185a711223ee6ce5f8b70d615c2
Minseon Song <songminseonk@gmail.com>
2014-05-28 00:01:28 -0700

    Got the basic game functionality set up.

--- js/game.js
+++ js/game.js
@@ -1,7 +1,86 @@
+
 $(document).ready(function(){
-	$("button").on('click', function(){
-		alert("I'm working!")
-	});
-
-
-});
+	var answer = Math.ceil(Math.random()*100);
+var number = [];
+var dif, d;
+function diff(a,b){
+	return Math.abs(b-a);
+}
+	alert("the answer is " + answer);
+		$('#submit').on('click', function(){
+			number = $('#guess_txt').val();
+			d = answer - number;
+			if(number<1 || number >101)
+			{
+				alert("Invalid number! Please enter from 1 to 100.");
+			}
+			else
+			{
+				dif = diff(number,answer);
+				if (dif == 0){
+					alert("Congratulations! you are correct.")
+				}
+				else if(dif<5)
+				{
+					alert("You are Boiling Hot");
+					if(d>0)
+					{
+						alert("guess higher");
+					}
+					else
+					{
+						alert("guess lower");
+					}
+				}
+				else if(dif < 15)
+				{
+					alert("You are hot");
+					if(d>0)
+					{
+						alert("guess higher");
+					}
+					else
+					{
+						alert("guess lower");
+					}
+				}
+				else if(dif <35)
+				{
+					alert("you are luck warm");
+					if(d>0)
+					{
+						alert("guess higher");
+					}
+					else
+					{
+						alert("guess lower");
+					}
+				}
+				else if(dif <65)
+				{
+					alert("You are cold");
+					if(d>0)
+					{
+						alert("guess higher");
+					}
+					else
+					{
+						alert("guess lower");
+					}
+				}
+				else
+				{
+					alert("You are freezing cold");
+					if(d>0)
+					{
+						alert("guess higher");
+					}
+					else
+					{
+						alert("guess lower");
+					}
+				}
+			}
+		return false;
+		})
+})
