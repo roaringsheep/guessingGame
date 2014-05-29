0455ea0e2698fcd4874814a55d89ebb5eda81204
Minseon Song <songminseonk@gmail.com>
2014-05-28 18:11:29 -0700

    Added reset button. being able to track guesses and prints text to screen instead of alerts.

--- js/game.js
+++ js/game.js
@@ -2,85 +2,101 @@
 $(document).ready(function(){
 	var answer = Math.ceil(Math.random()*100);
 var number = [];
-var dif, d;
+var dif, d, i=0;
 function diff(a,b){
 	return Math.abs(b-a);
 }
 	alert("the answer is " + answer);
 		$('#submit').on('click', function(){
-			number = $('#guess_txt').val();
-			d = answer - number;
-			if(number<1 || number >101)
+			number[i] = $('#guess_txt').val();
+			$("#sofar").text(number);
+			d = answer - number[i];
+			if(number[i]<1 || number[i] >101)
 			{
-				alert("Invalid number! Please enter from 1 to 100.");
+				$("#status").text("Invalid number! Please enter from 1 to 100.");
+				$("#heat").text("");
 			}
 			else
 			{
-				dif = diff(number,answer);
-				if (dif == 0){
-					alert("Congratulations! you are correct.")
+				dif = diff(number[i],answer);
+				if (dif == 0)
+				{
+					$("#status").text("Congratulations! You guessed correct.")
+					$("#heat").text("");
 				}
 				else if(dif<5)
 				{
-					alert("You are Boiling Hot");
+					$("#status").text("You are Boiling Hot");
 					if(d>0)
 					{
-						alert("guess higher");
+						$("#heat").text("guess higher");
 					}
 					else
 					{
-						alert("guess lower");
+						$("#heat").text("guess lower");
 					}
 				}
 				else if(dif < 15)
 				{
-					alert("You are hot");
+					$("#status").text("You are hot");
 					if(d>0)
 					{
-						alert("guess higher");
+						$("#heat").text("guess higher");
 					}
 					else
 					{
-						alert("guess lower");
+						$("#heat").text("guess lower");
 					}
 				}
 				else if(dif <35)
 				{
-					alert("you are luck warm");
+					$("#status").text("you are luck warm");
 					if(d>0)
 					{
-						alert("guess higher");
+						$("#heat").text("guess higher");
 					}
 					else
 					{
-						alert("guess lower");
+						$("#heat").text("guess lower");
 					}
 				}
 				else if(dif <65)
 				{
-					alert("You are cold");
+					$("#status").text("You are cold");
 					if(d>0)
 					{
-						alert("guess higher");
+						$("#heat").text("guess higher");
 					}
 					else
 					{
-						alert("guess lower");
+						$("#heat").text("guess lower");
 					}
 				}
-				else
+				else if (dif >= 65)
 				{
-					alert("You are freezing cold");
+					$("#status").text("You are freezing cold");
 					if(d>0)
 					{
-						alert("guess higher");
+						$("#heat").text("guess higher");
 					}
 					else
 					{
-						alert("guess lower");
+						$("#heat").text("guess lower");
 					}
 				}
 			}
-		return false;
+			if(i==4)
+			{
+				$("#status").text('You used up all your chances! The answer is ' + answer + ".");
+				$("#heat").text("");
+			}
+			else
+			{
+				i++;
+			}
+				return false;
 		})
+$("#reset").on('click', function(){
+				location.reload();
+			})
 })
